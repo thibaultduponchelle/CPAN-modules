07a133db46e7d16ee8603e5d944a9c89daaf04b7
thibaultduponchelle <580360+thibaultduponchelle@users.noreply.github.com>
2019-11-20 14:38:01 +0100

    Create LSD.pm

---
+++ Acme/LSD.pm
@@ -0,0 +1,27 @@
+package Acme::LSD;
+
+use base qw<Tie::Handle>;
+use Symbol qw<geniosym>;
+
+sub TIEHANDLE { return bless geniosym, __PACKAGE__ }
+
+sub PRINT {
+    shift;
+
+    foreach my $str (@_) {
+	      my $copy = $str;
+	      $copy =~ s/[^\w']/ /g;   # convert all non-words into spaces
+	      $copy =~ s/ +/ /g;       # convert all multiple spaces into single space
+	      $copy =~ tr/A-Z/a-z/;    # convert all words to lowercase
+	      foreach my $char (split("", $copy)) {
+	          my $r = int(rand(6)) + 31;
+	          my $s = int(rand(8));
+            print $OLD_STDOUT "\033[" . "$s;$r" . "m$char\033[0m";
+        }
+    }	
+}
+
+tie *PRINTOUT, 'Acme::LSD';
+our $OLD_STDOUT = select( *PRINTOUT );
+
+1;
