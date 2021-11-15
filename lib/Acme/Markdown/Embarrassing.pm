18997de372912db25c438910c75c21a9060c883d
Thibault DUPONCHELLE <thibault.duponchelle@amadeus.com>
2021-11-15 14:20:09 +0100

    MANIFEST

--- lib/Acme/Markdown/Embarrassing.pm
+++ lib/Acme/Markdown/Embarrassing.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '2.8';
+our $VERSION = '2.9';
 
 =format markdown
 
@@ -18,8 +18,8 @@ This is a toy module to embarrass MetaCPAN Markdown
 
 # MARKDOWN
 
-See also [README.md](https://metacpan.org/release/CONTRA/Acme-Markdown-Embarrassing-2.8/source/README.md)
-See also [README.md](https://metacpan.org/release/CONTRA/Acme-Markdown-Embarrassing-2.8/source/MarkdownTest.md)
+See also [README.md](https://metacpan.org/release/CONTRA/Acme-Markdown-Embarrassing-2.9/source/README.md)
+See also [README.md](https://metacpan.org/release/CONTRA/Acme-Markdown-Embarrassing-2.9/source/MarkdownTest.md)
 
 # Acme::Markdown::Embarrasing
 
